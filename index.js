f3ced31612d2957536ef1b51be853544f57a2056
zainah7 <zainaabd7@gmail.com>
2022-12-09 22:34:30 +0100

    Week 11 Assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+let userName;
+let age;
+
+userName = prompt("Please enter your userName", "Zainah Saheed");
+age= prompt("Please input your age");
+
+alert("Welcome, " +userName+ " ,You are "+age+ " years old.")
